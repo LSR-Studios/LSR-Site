307a3012230422c7d6efa41b148baac1847aac31
Lvvyh <170894576+lvvyh-roblox@users.noreply.github.com>
2024-06-27 21:13:04 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,30 @@
+const mysql = require('mysql')
+const numeral = require('numeral')
 
+const connection = mysql.createConnection({
+    host: 'mysql.db.bot-hosting.net',
+    port: 3306,
+    user: 'u94877_3YOVsThJdw',
+    password: 'kAOki+vjLYzS!Iz2gm+vUOei',
+    database: 's94877_Levels',
+});
+
+function getLeaderboard() {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM users ORDER BY exp DESC LIMIT 10`;
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.error(err);
+                return reject(err);
+            }
+            resolve(results);
+        });
+    });
+}
+
+let pTest = document:GetElementById('test')
+let text = ''
+getLeaderboard()
+  .then(results => {
+    console.log(results)
+  })
